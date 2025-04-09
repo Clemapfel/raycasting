9130d3b98500344b25091a677ee8bb937a213087
Clem Cords <mail@clemens-cords.com>
2025-04-09 21:38:04 +0200

    stashing

--- physics/spring.lua
+++ physics/spring.lua
@@ -3,37 +3,86 @@ b2.Spring = meta.class("Spring")
 
 --- @brief
 function b2.Spring:instantiate(body_a, body_b, x1, y1, x2, y2)
+    local ax, ay = body_a:get_position()
+    local bx, by = body_b:get_position()
+    meta.install(self, {
+        _body_a = body_a,
+        _body_b = body_b,
+        _x1 = x1 - ax,
+        _y1 = y1 - ay,
+        _x2 = x2 - bx,
+        _y2 = y2 - by,
+
+        _is_disabled = false
+    })
+
+    self:_initialize()
+end
+
+--- @brief
+function b2.Spring:_initialize()
+    local ax, ay = self._body_a:get_position()
+    local bx, by = self._body_b:get_position()
+
+    local x1, y1 = self._x1 + ax, self._y1 + ay
+    local x2, y2 = self._x2 + bx, self._y2 + by
     meta.install(self, {
         _prismatic_joint = love.physics.newPrismaticJoint(
-            body_a:get_native(),
-            body_b:get_native(),
+            self._body_a:get_native(),
+            self._body_b:get_native(),
             x1, y1,
             x2 - x1, y2 - y1,
             false
         ),
 
         _distance_joint = love.physics.newDistanceJoint(
-            body_a:get_native(),
-            body_b:get_native(),
+            self._body_a:get_native(),
+            self._body_b:get_native(),
             x1, y1,
             x2, y2,
             false
-        )
+        ),
     })
-
-    self._a_native = body_a:get_native()
-    self._b_native = body_b:get_native()
-    self._ax, self._ay = body_a:get_native():getLocalPoint(x1, y1)
-    self._bx, self._by = body_b:get_native():getLocalPoint(x2, y2)
 
     self._prismatic_joint:setLimitsEnabled(true)
     local distance = math.distance(x1, y1, x2, y2)
     self._prismatic_joint:setLimits(distance, distance)
-
-    self._distance_joint:setStiffness(1000)
 end
 
 --- @brief
 function b2.Spring:get_distance()
+    if self._is_disabled then return 0 end
     return math.abs(self._prismatic_joint:getJointTranslation())
+end
+
+--- @brief
+function b2.Spring:set_enabled(b)
+    self._prismatic_joint:setLimitsEnabled(b)
+
+    --[[
+    local ax, ay = self._body_a:get_position()
+    local bx, by = self._body_b:get_position()
+    local x1, y1 = self._x1 + ax, self._y1 + ay
+    local x2, y2 = self._x2 + bx, self._y2 + by
+    local distance = math.distance(x1, y1, x2, y2)
+
+    if not b then
+        self._prismatic_joint:setLimits(100 * distance, 100 * distance)
+    else
+        self._prismatic_joint:setLimits(distance, distance)
+    end
+    ]]--
+
+    --[[
+    if b == false and self._is_disabled == false then
+        self._prismatic_joint:destroy()
+        self._prismatic_joint = nil
+        self._distance_joint:destroy()
+        self._distance_joint = nil
+        self._is_disabled = true
+    elseif b == true and self._is_disabled == true then
+        self:_initialize()
+        self._is_disabled = false
+    end
+    ]]--
 end
