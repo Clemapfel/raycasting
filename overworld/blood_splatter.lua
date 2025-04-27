764a77aa4fbffd461c20510afd32958470fbf252
Clem Cords <mail@clemens-cords.com>
2025-04-27 20:33:19 +0200

    refactored, now has upper limits for n draws

--- overworld/blood_splatter.lua
+++ overworld/blood_splatter.lua
@@ -8,105 +8,95 @@ ow.BloodSplatter = meta.class("BloodSplatter")
 --- @brief
 function ow.BloodSplatter:instantiate()
     meta.install(self, {
-        _to_draw = {}
+        _edges = {},
+        _active_divisions = {}
     })
 end
 
-local _current_hue = 0
+function _overlap(x1, y1, x2, y2, cx, cy, radius)
+    -- overlap of segment xy and segment c - radius, c + radius
+    -- with c - radius, c + radius clamped to only lie on xy
+    local dx, dy = math.normalize(x1 - x2, y1 - y2)
 
-local sensor_body, sensor_shape
-
-function _intersection(x1, y1, x2, y2, cx, cy, radius)
-    local dx, dy = x2 - x1, y2 - y1
-    local A = dx * dx + dy * dy
-    local B = 2 * (dx * (x1 - cx) + dy * (y1 - cy))
-    local C = (x1 - cx) * (x1 - cx) + (y1 - cy) * (y1 - cy) - radius * radius
-
-    local discriminant = B * B - 4 * A * C
-
-    if discriminant < 0 then
-        return false
+    local dx_up, dy_up = dx * radius, dy * radius
+    local up_distance = math.distance(cx, cy, x1, y1)
+    if math.magnitude(dx_up, dy_up) > up_distance then
+        dx_up = dx * up_distance
+        dy_up = dy * up_distance
     end
 
-    local sqrtDiscriminant = math.sqrt(discriminant)
-    local t1 = (-B + sqrtDiscriminant) / (2 * A)
-    local t2 = (-B - sqrtDiscriminant) / (2 * A)
+    local dx_down, dy_down = dx * radius, dy * radius
+    local down_distance = math.distance(cx, cy, x2, y2)
+    if math.magnitude(dx_down, dy_down) > down_distance then
+        dx_down = dx * down_distance
+        dy_down = dy * down_distance
+    end
 
-    return true, x1 + t1 * dx, y1 + t1 * dy,
-        x1 + t2 * dx, y1 + t2 * dy
+    local ix1, iy1 = cx + dx_up, cy + dy_up
+    local ix2, iy2 = cx - dx_down, cy - dy_down
+
+    return ix1, iy1, ix2, iy2
 end
 
-local to_draw = {}
-
 --- @brief
-function ow.BloodSplatter:add(x, y, hue)
-    _current_hue = hue
-
+function ow.BloodSplatter:add(x, y, radius, hue)
     local r = 0.5
     self._world:queryShapesInArea(x - r, y - r, x + r, y + r, function(shape)
         local data = shape:getUserData()
         if data ~= nil then
             local x1, y1, x2, y2 = table.unpack(data.line)
-            local cx, cy, radius = x, y, rt.settings.overworld.blood_splatter.sensor_radius
-            local success, ix1, iy1, ix2, iy2 = _intersection(x1, y1, x2, y2, cx, cy, radius)
+            local cx, cy = x, y
+            local ix1, iy1, ix2, iy2 = _overlap(x1, y1, x2, y2, cx, cy, radius)
 
-            if success then
-                if ix1 and iy1 and ix2 and iy2 then
-                    ix1 = math.max(math.min(ix1, x2), x1)
-                    iy1 = math.max(math.min(iy1, y2), y1)
-                    ix2 = math.max(math.min(ix2, x2), x1)
-                    iy2 = math.max(math.min(iy2, y2), y1)
-                end
+            -- get left and right bounds as fraction in [0, 1]
+            local distance_1 = math.distance(ix1, iy1, x1, y1)
+            local distance_2 = math.distance(ix2, iy2, x1, y1)
 
-                if ix1 ~= nil then
-                    table.insert(to_draw, {
-                        { rt.lcha_to_rgba(rt.LCHA(0.8, 1, _current_hue, 1):unpack()) },
-                        { ix1, iy1, ix2, iy2 }
-                    })
-                end
+            if distance_2 < distance_1 then
+                distance_2 = math.distance(ix1, iy1, x2, y2)
+                distance_1 = math.distance(ix2, iy2, x2, y2)
             end
 
-            --data.color = { rt.lcha_to_rgba(rt.LCHA(0.8, 1, _current_hue, 1):unpack()) }
-            --data.lines = { ix1, iy1, ix2, iy2 }
-            --self._active_edges[shape] = true
+            local length = math.distance(x1, y1, x2, y2)
+            local left_fraction = distance_1 / length
+            local right_fraction = distance_2 / length
+            assert(left_fraction <= right_fraction)
+
+            local color = { rt.lcha_to_rgba(rt.LCHA(0.8, 1, hue, 1):unpack()) }
+
+            for division in values(data.subdivisions) do
+                -- check if segment overlaps interval
+                local left_f, right_f = division.left_fraction, division.right_fraction
+                if (left_f >= left_fraction and left_f <= right_fraction) or (right_f >= left_fraction and right_f <= right_fraction) then
+                    division.color = color
+                    if not division.is_active then
+                        self._active_divisions[division] = true
+                        division.is_active = true
+                    end
+                end
+            end
         end
+
         return true
     end)
-
-    --[[
-    if sensor_body == nil then
-        sensor_body = love.physics.newBody(self._world, x, y, b2.BodyType.KINEMATIC)
-        sensor_shape = love.physics.newCircleShape(sensor_body, rt.settings.overworld.blood_splatter.sensor_radius)
-        sensor_body:setBullet(true)
-    else
-        sensor_body:setPosition(x, y)
-    end
-
-    self._world:update(0)
-    ]]--
 end
 
 --- @brief
 function ow.BloodSplatter:draw()
-    --[[
     love.graphics.setLineWidth(2)
-    for edge in keys(self._active_edges) do
-        local data = edge:getUserData()
-        love.graphics.setColor(table.unpack(data.color))
-        love.graphics.line(data.line)
-    end
-    ]]--
-
-    love.graphics.setLineWidth(2)
-    for entry in values(to_draw) do
-        love.graphics.setColor(table.unpack(entry[1]))
-        love.graphics.line(entry[2])
+    for division in keys(self._active_divisions) do
+        love.graphics.setColor(table.unpack(division.color))
+        love.graphics.line(division.line)
     end
 end
 
+local _round = function(x)
+    return math.floor(x)
+end
+
 local _hash = function(points)
-    local x1, y1, x2, y2 = points[1], points[2], points[3], points[4]
-    if x1 > x2 or (x1 == x2 and y1 > y2) then -- swap so point order does not matter
+    local x1, y1, x2, y2 = _round(points[1]), _round(points[2]), _round(points[3]), _round(points[4])
+    if x1 < x2 or (x1 == x2 and y1 < y2) then -- swap so point order does not matter
         x1, y1, x2, y2 = x2, y2, x1, y1
     end
     return tostring(x1) .. "," .. tostring(y1) .. "," .. tostring(x2) .. "," .. tostring(y2)
@@ -118,7 +108,8 @@ end
 
 --- @brief
 function ow.BloodSplatter:create_contour(segments)
-    -- get all tuples of connected points, then filter unique
+    -- get all tuples of connected points, then filter unique to only get outer contours
+
     local tuples = {}
     local n_total = 0
     for segment in values(segments) do
@@ -138,12 +129,15 @@ function ow.BloodSplatter:create_contour(segments)
 
     self._edge_body = love.physics.newBody(self._world, 0, 0, b2.BodyType.STATIC)
 
-    local max_length = math.huge
+    local max_length = rt.settings.overworld.player.radius / 2
     for hash, count in pairs(tuples) do
         if count == 1 then
             local x1, y1, x2, y2 = _unhash(hash)
             local dx, dy = x2 - x1, y2 - y1
             local length = math.sqrt(dx * dx + dy * dy)
+
+            local edge = love.physics.newEdgeShape(self._edge_body, x1, y1, x2, y2)
+            local subdivisions = {}
 
             if length > max_length then
                 local num_segments = math.ceil(length / max_length)
@@ -157,33 +151,45 @@ function ow.BloodSplatter:create_contour(segments)
                     local sx2 = x1 + (i + 1) * segment_dx
                     local sy2 = y1 + (i + 1) * segment_dy
 
-                    local edge = love.physics.newEdgeShape(self._edge_body, sx1, sy1, sx2, sy2)
-                    edge:setUserData({
-                        color = {0, 0, 0, 1},
-                        line = {sx1, sy1, sx2, sy2}
+                    -- precompute left and right fraction for overlap test
+                    local left_fration = math.distance(sx1, sy1, x1, y1)
+                    local right_fraction = math.distance(sx2, sy2, x1, y1)
+
+                    if right_fraction < left_fration then
+                        right_fraction = math.distance(sx1, sy1, x2, y2)
+                        left_fration = math.distance(sx2, sy2, x2, y2)
+                    end
+
+                    left_fration = left_fration / length
+                    right_fraction = right_fraction / length
+
+                    table.insert(subdivisions, {
+                        line = {sx1, sy1, sx2, sy2},
+                        left_fraction = left_fration,
+                        right_fraction = right_fraction,
+                        is_active = false,
+                        color = nil
                     })
-                    table.insert(self._edges, edge)
                 end
             else
-                local edge = love.physics.newEdgeShape(self._edge_body, x1, y1, x2, y2)
-                edge:setUserData({
-                    color = {0, 0, 0, 1},
-                    line = {x1, y1, x2, y2}
+                table.insert(subdivisions, {
+                    line = {x1, y1, x2, y2},
+                    left_fraction = 0,
+                    right_fraction = 1,
+                    is_active = false,
+                    color = nil
                 })
-                table.insert(self._edges, edge)
             end
+
+            edge:setUserData({
+                line = {x1, y1, x2, y2},
+                subdivisions = subdivisions
+            })
         end
     end
+end
 
-    --[[
-    self._world:setCallbacks(function(shape_a, shape_b, contact)
-        for shape in range(shape_a, shape_b) do
-            local data = shape:getUserData()
-            if data ~= nil then
-                data.color = { rt.lcha_to_rgba(rt.LCHA(0.8, 1, _current_hue, 1):unpack()) }
-                self._active_edges[shape] = true
-            end
-        end
-    end)
-    ]]--
+--- @brief
+function ow.BloodSplatter:destroy()
+    self._world:destroy()
 end
