a37bb76c06d6cfc4e7345d617540c23142c67c34
Clem Cords <mail@clemens-cords.com>
2025-07-29 20:47:20 +0200

    update kill plane graphics

--- overworld/objects/kill_plane.glsl
+++ overworld/objects/kill_plane.glsl
@@ -1,3 +1,5 @@
+#ifdef PIXEL
+
 #define MODE_INNER 0
 #define MODE_OUTER 1
 
@@ -29,29 +31,6 @@ float gradient_noise(vec3 p) {
     dot( -1 + 2 * random_3d(i + vec3(1.0,1.0,1.0)), v - vec3(1.0,1.0,1.0)), u.x), u.y), u.z );
 }
 
-// Triangle wave function centered at zero
-float triangle(float x) {
-    return 2.0 * abs(fract(x) - 0.5) - 1.0;
-}
-
-// Noise function using summed triangle waves
-float triangle_noise(float x, int octaves, float persistence) {
-    float sum = 0.0;
-    float amp = 1.0;
-    float freq = 1.0;
-    float maxAmp = 0.0;
-
-    for (int i = 0; i < octaves; ++i) {
-        sum += amp * triangle(x * freq);
-        maxAmp += amp;
-        freq *= 2.0;
-        amp *= persistence;
-    }
-
-    // Normalize to [-1, 1]
-    return sum / maxAmp;
-}
-
 #define PI 3.1415926535897932384626433832795
 
 float gaussian(float x, float ramp)
@@ -60,8 +39,13 @@ float gaussian(float x, float ramp)
     return exp(((-4 * PI) / 3) * (ramp * x) * (ramp * x));
 }
 
+float symmetric(float value) {
+    return abs(fract(value) * 2.0 - 1.0);
+}
+
 uniform vec2 camera_offset;
 uniform float camera_scale = 1;
+
 vec2 to_uv(vec2 frag_position) {
     vec2 uv = frag_position;
     vec2 origin = vec2(love_ScreenSize.xy / 2);
@@ -75,31 +59,37 @@ vec2 to_uv(vec2 frag_position) {
 }
 
 uniform float elapsed;
+uniform vec4 red;
 
-const float noise_scale = 1;
+const float noise_scale = 30;
 
-#if MODE == MODE_INNER
-
-vec4 effect(vec4 color, sampler2D img, vec2 texture_coords, vec2 frag_position) {
-
-    vec2 uv = to_uv(frag_position);
-    float noise = gradient_noise(vec3(vec2(uv * 10) * noise_scale, elapsed));
-
-    return vec4(vec3(noise), 1);
-    return color;
+float discontinuous_dirac(float x) {
+    float a = 0.045 * exp(log(1.0 / 0.045 + 1.0) * x) - 0.045;
+    float b = 0.045 * exp(log(1.0 / 0.045 + 1.0) * (1.0 - x)) - 0.045;
+    const float t = 5.81894409826698685315796808094;
+    return t * min(a, b);
 }
 
-#elif MODE == MODE_OUTER
-
 vec4 effect(vec4 color, sampler2D img, vec2 texture_coords, vec2 frag_position) {
+    vec2 uv = to_uv(frag_position.xy);
 
-    float depth = 1 - texture_coords.y;
-    vec2 uv = to_uv(frag_position);
+    #if MODE == MODE_OUTER
 
-    float noise = gradient_noise(vec3(vec2(uv) * 30, elapsed));
+    vec2 seed = vec2(symmetric(texture_coords.x));
+    float noise = gradient_noise(vec3(vec2(seed) * noise_scale, elapsed));
+    float opacity = 1 - (texture_coords.y + noise);
+    vec3 intensity = vec3(1 - discontinuous_dirac(max(opacity, 0)));
 
-    float spikes = gaussian(1 - depth * ((noise * 2) + 1) / 2, 0.9);
-    return vec4(spikes);
+    #elif MODE == MODE_INNER
+
+    vec2 seed = vec2(texture_coords.xy);
+    float noise = gradient_noise(vec3(vec2(seed) * noise_scale, elapsed));
+    float opacity = noise;
+    vec3 intensity = vec3(1 - discontinuous_dirac(opacity));
+
+    #endif
+
+    return red * vec4(vec3(intensity), opacity);
 }
 
-#endif // MODE
+#endif // PIXEL
--- overworld/objects/kill_plane.lua
+++ overworld/objects/kill_plane.lua
@@ -1,7 +1,7 @@
 require "common.contour"
 
 rt.settings.overworld.kill_plane = {
-    spike_width = 20, -- px
+    border_width = 20
 }
 
 --- @class ow.KillPlane
@@ -9,34 +9,8 @@ ow.KillPlane = meta.class("KillPlane")
 
 local _inner_shader, _outer_shader
 
-local _mesh_format = {
-    { location = rt.VertexAttributeLocation.POSITION, name = rt.VertexAttribute.POSITION, format = "floatvec2" },
-    { location = rt.VertexAttributeLocation.TEXTURE_COORDINATES, name = rt.VertexAttribute.TEXTURE_COORDINATES, format = "floatvec2" },
-    { location = rt.VertexAttributeLocation.COLOR, name = rt.VertexAttribute.COLOR, format = "floatvec4" },
-}
-
-local function _segment_intersection(x1, y1, x2, y2, x3, y3, x4, y4)
-    local denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-
-    if denom == 0 then
-        return nil
-    end
-
-    local t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
-    local u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denom
-
-    if t >= 0 and t <= 1 and u >= 0 and u <= 1 then
-        local px = x1 + t * (x2 - x1)
-        local py = y1 + t * (y2 - y1)
-        return px, py
-    end
-
-    return nil
-end
-
 local first = true -- TODO
 
---- @brief
 function ow.KillPlane:instantiate(object, stage, scene)
     if _inner_shader == nil then _inner_shader = rt.Shader("overworld/objects/kill_plane.glsl", { MODE = 0 }) end
     if _outer_shader == nil then _outer_shader = rt.Shader("overworld/objects/kill_plane.glsl", { MODE = 1 }) end
@@ -44,19 +18,15 @@ function ow.KillPlane:instantiate(object, stage, scene)
     if first then
         self._input = rt.InputSubscriber()
         self._input:signal_connect("keyboard_key_pressed", function(_, which)
-            if which == "c" then
+            if which == "q" then
                 _inner_shader:recompile()
                 _outer_shader:recompile()
             end
         end)
-        first = false
     end
 
     self._scene = scene
     self._stage = stage
-
-    self._is_visible = object:get_boolean("is_visible", false)
-    if self._is_visible == nil then self._is_visible = true end
 
     -- collision
     self._body = object:create_physics_body(stage:get_physics_world())
@@ -77,86 +47,38 @@ function ow.KillPlane:instantiate(object, stage, scene)
         end)
     end)
 
+    -- visibility : disable mesh
+    self._is_visible = object:get_boolean("is_visible", false)
+    if self._is_visible == nil then self._is_visible = true end
     if self._is_visible == false then return end
 
-    -- compute center and mesh data
+    -- inner mesh solid
     self._contour = object:create_contour()
-    local center_x, center_y, n = 0, 0, 0
-    for i = 1, #self._contour, 2 do
-        local x, y = self._contour[i], self._contour[i+1]
-        center_x = center_x + x
-        center_y = center_y + y
-        n = n + 1
-    end
-
-    center_x = center_x / n
-    center_y = center_y / n
-    self._contour_center_x, self._contour_center_y = center_x, center_y
+    self._inner_mesh = object:create_mesh()
 
     table.insert(self._contour, self._contour[1])
     table.insert(self._contour, self._contour[2])
 
-    local inner_mesh_data = {}
+    -- Calculate arc-length parameterization for texture coordinates
+    local arc_lengths = {0} -- cumulative distances
+    local total_perimeter = 0
 
-    if rt.is_contour_convex(self._contour) then
-        local vertex_map = {}
-        inner_mesh_data = {
-            { self._contour_center_x, self._contour_center_y, 0, 0, 1, 1, 1, 1 }
-        }
-
-        local vertex_i = 1
-        for i = 1, #self._contour, 2 do
-            table.insert(inner_mesh_data, {
-                self._contour[i+0], self._contour[i+1], 1, 1, 1, 1, 1, 1
-            })
-        end
-
-        self._inner_mesh = rt.Mesh(
-            inner_mesh_data,
-            rt.MeshDrawMode.TRIANGLE_FAN,
-            _mesh_format,
-            rt.GraphicsBufferUsage.STATIC
-        )
-    else
-        local temp = table.deepcopy(self._contour)
-        table.insert(temp, 1, self._contour_center_y)
-        table.insert(temp, 1, self._contour_center_x)
-
-        local triangulation = rt.DelaunayTriangulation(temp, temp)
-
-        for tri in values(triangulation:get_triangles()) do
-            for i = 1, #tri, 2 do
-                local x, y = tri[i+0], tri[i+1]
-
-                local data
-                if math.equals(x, self._contour_center_x, math.eps) and
-                    math.equals(y, self._contour_center_y, math.eps)
-                then
-                    data = {
-                        x, y, 0, 0, 1, 1, 1, 1
-                    }
-                else
-                    data = {
-                        x, y, 1, 1, 1, 1, 1, 1
-                    }
-                end
-
-                table.insert(inner_mesh_data, data)
-            end
-        end
-
-        self._inner_mesh = rt.Mesh(
-            inner_mesh_data,
-            rt.MeshDrawMode.TRIANGLES,
-            _mesh_format,
-            rt.GraphicsBufferUsage.STATIC
-        )
+    for i = 1, #self._contour - 2, 2 do
+        local x1, y1 = self._contour[i+0], self._contour[i+1]
+        local x2, y2 = self._contour[i+2], self._contour[i+3]
+        local segment_length = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+        total_perimeter = total_perimeter + segment_length
+        table.insert(arc_lengths, total_perimeter)
     end
 
-    -- outer mesh
+    -- outer mesh border
+
+    self._outer_mesh = nil
+
     local outer_mesh_data = {}
-    local vertex_map = {}
-    local border_w = rt.settings.overworld.kill_plane.spike_width
+    local outer_mesh_vertex_map = {}
+
+    local border_w = rt.settings.overworld.kill_plane.border_width
 
     local _contour_x1, _contour_y1, _contour_x2, _contour_y2 = 1, 2, 3, 4
     local _outer_x1, _outer_y1, _outer_x2, _outer_y2 = 5, 6, 7, 8
@@ -184,7 +106,24 @@ function ow.KillPlane:instantiate(object, stage, scene)
         })
     end
 
-    self._dbg = {}
+    local function _segment_intersection(x1, y1, x2, y2, x3, y3, x4, y4)
+        local denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+        if denom == 0 then
+            return nil
+        end
+
+        local t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
+        local u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denom
+
+        if t >= 0 and t <= 1 and u >= 0 and u <= 1 then
+            local px = x1 + t * (x2 - x1)
+            local py = y1 + t * (y2 - y1)
+            return px, py
+        end
+
+        return nil
+    end
 
     local vertex_i = 1
     for quad_i = 1, #quads do
@@ -201,28 +140,31 @@ function ow.KillPlane:instantiate(object, stage, scene)
             next[_outer_x1], next[_outer_y1] = ix, iy
         end
 
+        -- Calculate normalized arc-length texture coordinates
+        local u1 = arc_lengths[quad_i] / total_perimeter
+        local u2 = arc_lengths[quad_i + 1] / total_perimeter
 
         table.insert(outer_mesh_data, {
-            current[_contour_x1], current[_contour_y1], 0, 0, 1, 1, 1, 1
+            current[_contour_x1], current[_contour_y1], u1, 0, 1, 1, 1, 1
         })
 
         table.insert(outer_mesh_data, {
-            current[_contour_x2], current[_contour_y2], 1, 0, 1, 1, 1, 1
+            current[_contour_x2], current[_contour_y2], u2, 0, 1, 1, 1, 1
         })
 
         table.insert(outer_mesh_data, {
-            current[_outer_x1], current[_outer_y1], 0, 1, 0, 0, 0, 1
+            current[_outer_x1], current[_outer_y1], u1, 1, 0, 0, 0, 1
         })
 
         table.insert(outer_mesh_data, {
-            current[_outer_x2], current[_outer_y2], 1, 1, 0, 0, 0, 1
+            current[_outer_x2], current[_outer_y2], u2, 1, 0, 0, 0, 1
         })
 
         for j in range(
             vertex_i + 0, vertex_i + 1, vertex_i + 2,
             vertex_i + 1, vertex_i + 2, vertex_i + 3
         ) do
-            table.insert(vertex_map, j)
+            table.insert(outer_mesh_vertex_map, j)
         end
 
         vertex_i = vertex_i + 4
@@ -235,43 +177,52 @@ function ow.KillPlane:instantiate(object, stage, scene)
             i + 3, -- current outer
             i + 4 + 2 -- next outer
         ) do
-            table.insert(vertex_map, j)
+            table.insert(outer_mesh_vertex_map, j)
         end
     end
 
-    for j in range(
-        vertex_i - 4 + 1,
-        vertex_i - 4 + 3,
-        3
-    ) do
-        table.insert(vertex_map, j)
+    do -- last triangle needs separate coords, otherwise it would interpolate between uv.x = 0 and uv.y = 1
+        local a = table.deepcopy(outer_mesh_data[vertex_i - 4 + 1])
+        local b = table.deepcopy(outer_mesh_data[vertex_i - 4 + 3])
+        local c = table.deepcopy(outer_mesh_data[3])
+
+        for which in range(a, b, c) do
+            which[3] = 1 -- override uv.x
+            table.insert(outer_mesh_data, which)
+
+            table.insert(outer_mesh_vertex_map, vertex_i)
+            vertex_i = vertex_i + 1
+        end
     end
 
     self._outer_mesh = rt.Mesh(
         outer_mesh_data,
-        rt.MeshDrawMode.TRIANGLES,
-        _mesh_format,
-        rt.GraphicsBufferUsage.STATIC
+        rt.MeshDrawMode.TRIANGLES
     )
-    self._outer_mesh:set_vertex_map(vertex_map)
+    self._outer_mesh:set_vertex_map(outer_mesh_vertex_map)
 end
 
---- @brief
 function ow.KillPlane:draw()
     if not self._is_visible or not self._scene:get_is_body_visible(self._body) then return end
+
+    local camera_offset = { self._scene:get_camera():get_offset() }
+    local camera_scale = self._scene:get_camera():get_final_scale()
+    local red = { rt.Palette.RED:unpack() }
     love.graphics.setColor(1, 1, 1, 1)
 
     _inner_shader:bind()
-    _inner_shader:send("elapsed", rt.SceneManager:get_elapsed()) -- synched along all kill planes
-    _inner_shader:send("camera_offset", { self._scene:get_camera():get_offset() })
-    _inner_shader:send("camera_scale", self._scene:get_camera():get_final_scale())
+    _inner_shader:send("elapsed", rt.SceneManager:get_elapsed() + meta.hash(self) * 100)
+    _inner_shader:send("camera_offset", camera_offset)
+    _inner_shader:send("camera_scale", camera_scale)
+    _inner_shader:send("red", red)
     self._inner_mesh:draw()
     _inner_shader:unbind()
 
     _outer_shader:bind()
-    _outer_shader:send("elapsed", rt.SceneManager:get_elapsed())
-    _outer_shader:send("camera_offset", { self._scene:get_camera():get_offset() })
-    _outer_shader:send("camera_scale", self._scene:get_camera():get_final_scale())
+    _outer_shader:send("elapsed", rt.SceneManager:get_elapsed() + meta.hash(self) * 100)
+    _outer_shader:send("camera_offset", camera_offset)
+    _outer_shader:send("camera_scale", camera_scale)
+    _outer_shader:send("red", red)
     self._outer_mesh:draw()
     _outer_shader:unbind()
 end
