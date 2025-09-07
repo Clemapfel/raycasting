71310da53ddd67b198c423b0d625a978721958d0
Clem Cords <mail@clemens-cords.com>
2025-09-07 02:21:55 +0200

    stashing

--- overworld/objects/moving_hitbox.lua
+++ overworld/objects/moving_hitbox.lua
@@ -1,3 +1,9 @@
+require "common.path"
+require "common.contour"
+
+rt.settings.overworld.moving_hitbox = {
+    default_velocity = 100, -- px per second
+}
 
 --- @class ow.MovingHitbox
 ow.MovingHitbox = meta.class("MovingHitbox")
@@ -8,58 +14,139 @@ ow.MovingHitboxTarget = meta.class("MovingHitboxTarget")
 --- @brief
 function ow.MovingHitbox:instantiate(object, stage, scene)
     self._scene = scene
+    self._stage = stage
+
+    -- read path by iterating through nodes
+    local target = object:get_object("target", true)
+    local path = {}
+    repeat
+        assert(target:get_type() == ow.ObjectType.POINT, "In ow.MovingHitbox: `MovingHitboxTarget` (" .. object:get_id() .. ") is not a point")
+        table.insert(path, target.x)
+        table.insert(path, target.y)
+        target = target:get_object("target")
+    until target == nil
+
+    self._should_loop = object:get_boolean("should_loop", false)
+    if self._should_loop == nil then self._should_loop = false end
+
+    if self._should_loop then
+        table.insert(path, path[1])
+        table.insert(path, path[2])
+        self._path = rt.Path(path)
+    else
+        self._path = rt.Path(path)
+    end
+
+    self._x, self._y = self._path:at(0)
+
+    local centroid_x, centroid_y = object:get_centroid()
+    self._offset_x, self._offset_y = centroid_x - self._x, centroid_y - self._y
+    self._velocity = object:get_number("velocity", false) or rt.settings.overworld.moving_hitbox.default_velocity
+
+    local easing = rt.InterpolationFunctions.LINEAR
+    local easing_name = object:get_string("easing", false)
+    if easing_name ~= nil then
+        easing = rt.InterpolationFunctions[easing_name]
+        if easing == nil then
+            rt.error("In ow.MovingPlatform: for object `" .. object:get_id() .. "`: unknown easing `" .. easing_name .. "`")
+        end
+    end
+    self._easing = easing
 
     self._body = object:create_physics_body(stage:get_physics_world(), b2.BodyType.KINEMATIC)
     self._body:add_tag("stencil")
     self._body:set_collides_with(rt.settings.player.bounce_collision_group)
     self._body:set_collision_group(rt.settings.player.bounce_collision_group)
+    self._body:set_position(self._x, self._y) -- revert offset
 
-    local target = object:get_object("axis")
-    assert(target:get_type() == ow.ObjectType.POINT)
+    -- reset cycle on player respawn
+    self._start_timestamp = love.timer.getTime()
+    self._stage:signal_connect("respawn", function()
+        self._start_timestamp = love.timer.getTime()
+    end)
 
-    self._x, self._y = object:get_centroid()
-    self._dx, self._dy = math.normalize(target.x - self._x, target.y - self._y)
-    self._length = math.distance(self._x, self._y, target.x, target.y)
-    self._lower = object:get_number("lower") or 0
-    self._upper = object:get_number("upper") or 1
+    -- mesh
+    local _, tris, mesh_prototype
+    _, tris, mesh_prototype = object:create_mesh()
 
-    self._elapsed = 0
-    self._speed = 50
+    -- subtract from origin
+    for tri in values(tris) do
+        for i = 1, 6, 2 do
+            tri[i+0] = tri[i+0] - centroid_x
+            tri[i+1] = tri[i+1] - centroid_y
+        end
+    end
+
+    for vertex in values(mesh_prototype) do
+        vertex[1] = vertex[1] - centroid_x
+        vertex[2] = vertex[2] - centroid_y
+    end
+
+    self._mesh = rt.Mesh(mesh_prototype, rt.MeshDrawMode.TRIANGLES)
+
+    self._contour = rt.contour_from_tris(tris)
+    table.insert(self._contour, self._contour[1])
+    table.insert(self._contour, self._contour[2])
 end
 
---- @brief
-function ow.MovingHitbox:update(delta)
-    if not self._scene:get_is_body_visible(self._body) then return end
+local dt = math.eps * 10e2
 
-    self._elapsed = self._elapsed + delta
-    self._value = rt.InterpolationFunctions.TRIANGLE_WAVE(self._elapsed * (self._speed / self._length))
+function ow.MovingHitbox:update(_)
+    --if not self._scene:get_is_body_visible(self._body) then return end
 
-    local x1 = self._x + self._lower * self._dx * self._length
-    local y1 = self._y + self._lower * self._dy * self._length
-    local x2 = self._x + self._upper * self._dx * self._length
-    local y2 = self._y + self._upper * self._dy * self._length
+    local elapsed = (love.timer.getTime() - self._start_timestamp)
+    local length = self._path:get_length()
+    local t, direction, easing_derivative
 
-    local target_x, target_y = x1 + self._value * (x2 - x1), y1 + self._value * (y2 - y1)
-    target_x = target_x - self._x
-    target_y = target_y - self._y
-    local current_x, current_y = self._body:get_position()
-    self._body:set_linear_velocity(
-        (target_x - current_x) * self._speed,
-        (target_y - current_y) * self._speed
+    if self._should_loop then
+        local distance = self._velocity * elapsed
+        t = (distance / length) % 1.0
+        direction = 1
+
+        local easing_t = self._easing(t)
+        local easing_t_dt = self._easing((t + dt) % 1.0)
+        easing_derivative = (easing_t_dt - easing_t) / dt
+    else
+        local distance_in_cycle = (self._velocity * elapsed) % (2 * length)
+
+        if distance_in_cycle <= length then
+            -- going forwards
+            t = distance_in_cycle / length
+            direction = 1
+
+            local easing_t = self._easing(t)
+            local easing_t_dt = self._easing(math.min(t + dt, 1.0))
+            easing_derivative = (easing_t_dt - easing_t) / dt
+        else
+            -- going backwards
+            local backward_distance = distance_in_cycle - length
+            t = (length - backward_distance) / length
+            direction = -1
+
+            -- calculate derivative for backward motion (reversed easing)
+            local reversed_t = 1 - t
+            local easing_t = self._easing(math.max(reversed_t - dt, 0.0))
+            local easing_t_dt = self._easing(reversed_t)
+            easing_derivative = (easing_t_dt - easing_t) / dt
+        end
+    end
+
+    local dx, dy = self._path:get_tangent(t)
+    self._body:set_velocity(
+        dx * self._velocity * direction * easing_derivative,
+        dy * self._velocity * direction * easing_derivative
     )
-    --self._body:set_position(target_x, target_y)
 end
 
 --- @brief
 function ow.MovingHitbox:draw()
-    if not self._scene:get_is_body_visible(self._body) then return end
+    love.graphics.push()
+    love.graphics.translate(self._body:get_position())
 
-    love.graphics.setColor(1, 1, 1, 1)
-    self._body:draw()
-    local x1 = self._x + self._lower * self._dx
-    local y1 = self._y + self._lower * self._dy
-    local x2 = self._x + self._upper * self._dx
-    local y2 = self._y + self._upper * self._dy
-    love.graphics.line(x1, y1, x2, y2)
-    love.graphics.circle("fill", self._x, self._y, 10)
+    rt.Palette.RED:bind()
+    self._mesh:draw()
+
+    rt.Palette.WHITE:bind()
+    love.graphics.line(self._contour)
+    love.graphics.pop()
 end
