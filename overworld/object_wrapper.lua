d00f5513ab449cb77b83e3d519f81b6a6692339a
Clem Cords <mail@clemens-cords.com>
2025-09-16 19:48:33 +0200

    transform applied to mesh again

--- overworld/object_wrapper.lua
+++ overworld/object_wrapper.lua
@@ -423,7 +423,7 @@ function ow.ObjectWrapper:_initialize_mesh_prototype()
     self.mesh_prototype = {}
     self.mesh_triangles = {}
 
-    local polygonized = rt.math.triangulate(to_polygonize)--_process_polygon(to_polygonize, self))
+    local polygonized = rt.math.triangulate(_process_polygon(to_polygonize, self))
 
     for tri in values(polygonized) do
         for i = 1, #tri, 2 do
