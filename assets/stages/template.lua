93aab79bdd4137861dd6c9761ab8d32950609eaf
Clem Cords <mail@clemens-cords.com>
2025-10-25 03:23:23 +0200

    added template

---
+++ assets/stages/template.lua
@@ -0,0 +1,121 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 32,
+  height = 32,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 4,
+  nextobjectid = 7,
+  properties = {},
+  tilesets = {},
+  layers = {
+    {
+      type = "imagelayer",
+      image = "../tilesets/debug_tileset/Frame035.png",
+      id = 3,
+      name = "background",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      repeatx = true,
+      repeaty = true,
+      properties = {}
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "objects",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "",
+          type = "Hitbox",
+          shape = "polygon",
+          x = -96,
+          y = -128,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polygon = {
+            { x = 0, y = 0 },
+            { x = 1200, y = 0 },
+            { x = 1200, y = 672 },
+            { x = 0, y = 672 }
+          },
+          properties = {}
+        },
+        {
+          id = 2,
+          name = "Spawn",
+          type = "PlayerSpawn",
+          shape = "point",
+          x = 16,
+          y = -192,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 3,
+          name = "Goal",
+          type = "Goal",
+          shape = "point",
+          x = 992,
+          y = -176,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 5,
+          name = "Checkpoint",
+          type = "Checkpoint",
+          shape = "point",
+          x = 512,
+          y = -192,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 6,
+          name = "CameraBounds",
+          type = "CameraBounds",
+          shape = "rectangle",
+          x = -528,
+          y = -944,
+          width = 2048,
+          height = 1488,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
