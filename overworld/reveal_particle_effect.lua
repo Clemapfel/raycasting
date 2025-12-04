4cc8c3de7de70ab8ed061251225b83902e6cd470
Clem Cords <mail@clemens-cords.com>
2025-12-04 23:26:55 +0100

    particle effect done

--- overworld/reveal_particle_effect.lua
+++ overworld/reveal_particle_effect.lua
@@ -9,49 +9,17 @@ rt.settings.overworld.reveal_particle_effect = {
     gravity = 100,
     radius = 7,
 
-    particle_density = 0.5, -- per px
+    particle_density = 0.15, -- per px
     max_n_particles = 2000
 }
 
 --- @class ow.RevealParticleEffect
 ow.RevealParticleEffect = meta.class("RevealParticleEffect")
 
-local _particle_texture
-local _particle_texture_padding = 20
-
 --- @brief
 function ow.RevealParticleEffect:instantiate(...)
     self._batches = {}
     if select("#", ...) > 0 then self:emit(...) end
-
-    if _particle_texture == nil then
-        local w, h = 200, 200
-        local mesh
-
-        do
-            local radius = 0.5 * w
-            local border = 0.05 * radius -- anti-aliasing outer ring
-            local n_outer_vertices = 32
-            mesh = rt.MeshRing(0, 0, radius - border, radius, true, n_outer_vertices)
-            local n = mesh:get_n_vertices()
-            mesh:set_vertex_color(1, 1, 1, 1, 0)
-            for i = n, n - n_outer_vertices + 1, -1 do -- outer aliasing
-                mesh:set_vertex_color(i, 1, 1, 1, 0)
-            end
-            mesh:set_vertex_color(1, 1, 1, 1)
-        end
-
-        local canvas_w, canvas_h = w + 2 * _particle_texture_padding, h + 2 * _particle_texture_padding
-        _particle_texture = rt.RenderTexture(canvas_w, canvas_h, 4)
-        love.graphics.push("all")
-        love.graphics.reset()
-        _particle_texture:bind()
-        love.graphics.clear(0, 0, 0, 0)
-        love.graphics.translate(0.5 * canvas_w, 0.5 * canvas_h)
-        mesh:draw()
-        _particle_texture:unbind()
-        love.graphics.pop()
-    end
 end
 
 local _x = 1
@@ -62,8 +30,20 @@ local _mass = 5
 local _color_r = 6
 local _color_g = 7
 local _color_b = 8
-local _rotation = 9
-local _n_segments = 10
+local _segments = 10
+
+local _circle = function(radius, offset, n_segments)
+    local points = {}
+    for i = 1, n_segments do
+        local angle = (i - 1) / n_segments * 2 * math.pi + offset
+        table.insert(points, math.cos(angle) * radius)
+        table.insert(points, math.sin(angle) * radius)
+    end
+
+    table.insert(points, points[1])
+    table.insert(points, points[2])
+    return points
+end
 
 --- @brief
 function ow.RevealParticleEffect:emit(ax, ay, bx, by, side)
@@ -100,7 +80,6 @@ function ow.RevealParticleEffect:emit(ax, ay, bx, by, side)
         side = side > 0
     }
 
-
     for i = 1, n_particles do
         self._total_n_particles = self._total_n_particles + 1
         if self._total_n_particles < settings.max_n_particles then
@@ -110,6 +89,7 @@ function ow.RevealParticleEffect:emit(ax, ay, bx, by, side)
             local mass = rt.random.number(settings.min_mass, settings.max_mass)
             local hue = rt.random.number(0, 1)
             local r, g, b, _ = rt.lcha_to_rgba(0.8, 1, hue, 1)
+
             local particle = {
                 [_x] = x,
                 [_y] = y,
@@ -119,9 +99,11 @@ function ow.RevealParticleEffect:emit(ax, ay, bx, by, side)
                 [_color_r] = r,
                 [_color_g] = g,
                 [_color_b] = b,
-                [_rotation] = rt.random.number(0, 2 * math.pi),
-                [_n_segments] = rt.random.choose(3, 4, 5, 6),
-                [_segments] = points
+                [_segments] = _circle(
+                    mass * settings.radius,
+                    rt.random.number(0, 2 * math.pi), -- angle offset
+                    rt.random.choose(3, 4, 5, 6)
+                )
             }
 
             table.insert(batch.particles, particle)
@@ -144,7 +126,7 @@ function ow.RevealParticleEffect:update(delta)
         return rt.random.noise(x, y) * 2 - 1
     end
 
-    local gravity = rt.settings.overworld.reveal_particle_effect.gravity
+    local gravity = rt.settings.overworld.reveal_particle_effect.gravity * rt.get_pixel_scale()
 
     local batch_to_remove = {}
     for batch_i, batch in ipairs(self._batches) do
@@ -178,59 +160,44 @@ end
 
 --- @brief
 function ow.RevealParticleEffect:draw()
-    local black_r, black_g, black_b = rt.Palette.BLACK:unpack()
-    love.graphics.setLineWidth(1)
     local radius = rt.settings.overworld.reveal_particle_effect.radius * rt.get_pixel_scale()
-    local texture, texture_w, texture_h = _particle_texture:get_native(), _particle_texture:get_size()
-
-    local line_width = 1
-
-    love.graphics.push()
-    love.graphics.origin()
-    love.graphics.draw(texture)
-    love.graphics.pop()
+    local line_width = 2
 
     love.graphics.clear(0.5, 0.5, 0.5, 1)
-    love.graphics.push()
 
-    love.graphics.setLineWidth(line_width)
+    love.graphics.push("all")
 
-    --rt.graphics.set_blend_mode(rt.BlendMode.ADD, rt.BlendMode.NORMAL)
     local damping = 0.05
     for batch in values(self._batches) do
-        for particle in values(batch.particles) do
-            local particle_radius = radius * particle[_mass] * rt.get_pixel_scale()
 
-            --[[
-            local scale_x, scale_y = particle_radius / (texture_w - _particle_texture_padding), particle_radius / (texture_h - _particle_texture_padding)
-            love.graphics.setColor(particle[_color_r] * damping, particle[_color_g] * damping, particle[_color_b] * damping, 1)
-            love.graphics.draw(
-                texture,
-                particle[_x], particle[_y], 0,
-                scale_x, scale_y,
-                0.5 * texture_w, 0.5 * texture_h
-            )
-            ]]--
+        love.graphics.setLineWidth(line_width + 1)
+        love.graphics.setLineStyle("smooth")
+
+        for particle in values(batch.particles) do
             love.graphics.push()
             love.graphics.translate(particle[_x], particle[_y])
-            love.graphics.rotate(particle[_rotation])
+            love.graphics.scale(rt.get_pixel_scale(), rt.get_pixel_scale())
 
             rt.Palette.BLACK:bind()
-            love.graphics.circle("fill",
-                0, 0, particle_radius, particle[_n_segments]
-            )
-
-            love.graphics.setColor(particle[_color_r], particle[_color_g], particle[_color_b])
-            love.graphics.circle("line",
-                0, 0, particle_radius, particle[_n_segments]
-            )
+            love.graphics.line(particle[_segments])
 
             love.graphics.pop()
+        end
 
+        love.graphics.setLineWidth(line_width)
+        love.graphics.setLineStyle("rough")
+
+        for particle in values(batch.particles) do
+            love.graphics.push()
+            love.graphics.translate(particle[_x], particle[_y])
+            love.graphics.scale(rt.get_pixel_scale(), rt.get_pixel_scale())
+
+            love.graphics.setColor(particle[_color_r], particle[_color_g], particle[_color_b])
+            love.graphics.line(particle[_segments])
+
+            love.graphics.pop()
         end
     end
-
-    --rt.graphics.set_blend_mode(nil)
 
     love.graphics.pop()
 end
