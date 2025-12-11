92d5e40fad1c0a51b13385dbf8801dce725518ea
Clem Cords <mail@clemens-cords.com>
2025-12-11 18:45:28 +0100

    added stage preview

---
+++ menu/stage_preview.lua
@@ -0,0 +1,33 @@
+require "common.widget"
+
+rt.settings.menu.stage_preview = {
+
+}
+
+--- @class mn.StagePreview
+mn.StagePreview = meta.class("StagePreview", rt.Widget)
+
+--- @brief
+function mn.StagePreview:instantiate(stage_id)
+    self._contours = {}
+end
+
+--- @brief
+function mn.StagePreview:realize()
+
+end
+
+--- @brief
+function mn.StagePreview:size_allocate(x, y, width, height)
+
+end
+
+--- @brief
+function mn.StagePreview:draw()
+
+end
+
+--- @brief
+function mn.StagePreview:update(delta)
+
+end
