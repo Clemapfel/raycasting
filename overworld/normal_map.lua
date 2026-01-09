101cdf90ec06b899e5517a2dd4513312b0b7bc20
Clem Cords <mail@clemens-cords.com>
2026-01-09 23:44:20 +0100

    now correctly uses graphics state color

--- overworld/normal_map.lua
+++ overworld/normal_map.lua
@@ -529,7 +529,8 @@ function ow.NormalMap:draw_light(
     _draw_light_shader:bind()
     love.graphics.setBlendMode("add", "premultiplied")
     local r, g, b, a = love.graphics.getColor() -- premultiply alpha
-    love.graphics.setColor(r * a, g * a, b * a, a)
+    local value = math.max(r, g, b)
+    love.graphics.setColor(value * a, value * a, value * a, a)
 
     for chunk_x = min_chunk_x, max_chunk_x do
         local column = self._chunks[chunk_x]
