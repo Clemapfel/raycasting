242e85daa2964a295a00365914f114311fd29ac1
Clem Cords <mail@clemens-cords.com>
2026-01-16 20:08:43 +0100

    added kaizo fish

--- overworld/objects/kaizo_fish.lua
+++ overworld/objects/kaizo_fish.lua
@@ -1,3 +1,7 @@
+rt.settings.overworld.kaizo_fish = {
+    default_velocity = 100
+}
+
 --- @class ow.KaizoFish
 ow.KaizoFish = meta.class("KaizoFish")
 
@@ -12,12 +16,13 @@ function ow.KaizoFish:instantiate(object, stage, scene)
 
     self._body = object:create_physics_body(
         self._world,
-        b2.BodyType.DYNAMIC
+        b2.BodyType.KINEMATIC
     )
+    self._body:set_mass(1)
 
     self._start_velocity_x = object:get_number("velocity_x") or 0
     self._start_velocity_y = object:get_number("velocity_y") or 0
-    self._start_x, self._start_y = object:get_centroid()
+    self._start_x, self._start_y = self._body:get_position()
 
     self._sensor = object:get_object("sensor", true):create_physics_body(
         self._world,
@@ -30,33 +35,44 @@ function ow.KaizoFish:instantiate(object, stage, scene)
         body:set_collides_with(rt.settings.player.bounce_collision_group)
     end
 
-    self._stage:signal_connect("respawn", function(_)
+    local reset = function()
         self._body:set_position(self._start_x, self._start_y)
         self._body:set_velocity(0, 0)
-        self._is_active = false
+    end
+
+    reset()
+
+    self._stage:signal_connect("respawn", function(_)
+        reset()
     end)
 
-    self._sensor:signal_connect("collision_start", function(_, other_body)
+    self._stage:signal_connect("initialized", function()
+        reset()
+        return meta.DISCONNECT_SIGNAL
+    end)
+
+    self._sensor:signal_connect("collision_start", function(self_body, other_body)
         self._is_active = true
-    end)
+        self._body:set_position(self._start_x, self._start_y)
 
-    self._sensor:signal_connect("collision_end", function(_, other_body)
-        self._is_active = false
+        if self._start_velocity_x == 0 and self._start_velocity_y == 0 then
+            local px, py = self._scene:get_player():get_position()
+            local self_x, self_y = self_body:get_position()
+            local dx, dy = math.normalize(px - self._x, py - self_y)
+            local magnitude = rt.settings.overworld.kaizo_fish.default_velocity
+            self._body:set_velocity(dx * magnitude, dy * magnitude)
+        else
+            self._body:set_velocity(self._start_velocity_x, self._start_velocity_y)
+        end
     end)
 
     self._body:signal_connect("collision_start", function(_, other_body)
         if other_body:has_tag("player") then
-            self._scene:get_player():kill()
+            --self._scene:get_player():kill()
         end
     end)
 end
 
---- @brief
-function ow.KaizoFish:update(delta)
-    if self._is_active == true then
-        self._body:set_velocity(self._start_velocity_x, self._start_velocity_y)
-    end
-end
 
 --- @brief
 function ow.KaizoFish:draw()
