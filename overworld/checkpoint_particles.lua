644bc8279f7950f3afa45f06ebd504348a957707
Clem Cords <mail@clemens-cords.com>
2026-03-22 20:42:22 +0100

    stashing particles

--- overworld/checkpoint_particles.lua
+++ overworld/checkpoint_particles.lua
@@ -4,7 +4,7 @@ rt.settings.overworld.checkpoint_particles = {
     min_radius = 5,
     max_radius = 7,
 
-    min_velocity = 300,
+    min_velocity = 100,
     max_velocity = 600,
 
     gravity = 50,
@@ -15,8 +15,8 @@ rt.settings.overworld.checkpoint_particles = {
     min_angle = 0,
     max_angle = 2 * math.pi,
 
-    min_damping = 0.98,
-    max_damping = 0.99,
+    min_damping = 0.99,
+    max_damping = 0.992,
 
     n_particles = 200,
 
@@ -110,8 +110,8 @@ end
 
 local _position_x = 1
 local _position_y = 2
-local _velocity_dx = 3
-local _velocity_dy = 4
+local _velocity_x = 3
+local _velocity_y = 4
 local _mass = 5
 local _radius = 6
 local _damping = 7
@@ -153,8 +153,8 @@ function ow.CheckpointParticles:_init_batch(batch, origin_x, origin_y, settings)
         local particle = {
             [_position_x] = px,
             [_position_y] = py,
-            [_velocity_dx] = dx * velocity_magnitude,
-            [_velocity_dy] = dy * velocity_magnitude,
+            [_velocity_x] = dx * velocity_magnitude,
+            [_velocity_y] = dy * velocity_magnitude,
             [_mass] = rt.random.number(settings.min_mass, settings.max_mass),
             [_radius] = radius,
             [_damping] = rt.random.number(settings.min_damping, settings.max_damping),
@@ -199,9 +199,14 @@ end
 function ow.CheckpointParticles:_update_batch(batch, delta)
     local gravity_dx, gravity_dy, gravity = 0, 1, batch.settings.gravity
     local opacity_easing = rt.InterpolationFunctions.SINUSOID_EASE_OUT
-    local tail_easing = rt.InterpolationFunctions.LINEAR
     local bounds = self._bounds
     local despawn_when_leaving_screen = batch.settings.despawn_when_leaving_screen
+
+    local function tail_easing(x, t)
+        -- x: 0 to 1 head to tail
+        -- t: 1 to 0 tail to circle
+        return t * math.cos((math.pi / 2) * x) * math.exp(-x^2) + (1 - t) * math.sqrt(1 - x^2)
+    end
 
     local arc_offset = math.pi / 2
     local min_tail_length_factor = batch.settings.min_tail_length_factor
@@ -214,11 +219,11 @@ function ow.CheckpointParticles:_update_batch(batch, delta)
         end
 
         -- integrate position
-        p[_velocity_dx] = p[_damping] * (p[_velocity_dx] + (gravity_dx * gravity * delta))
-        p[_velocity_dy] = p[_damping] * (p[_velocity_dy] + (gravity_dy * gravity * delta))
+        p[_velocity_x] = p[_damping] * (p[_velocity_x] + (gravity_dx * gravity * delta))
+        p[_velocity_y] = p[_damping] * (p[_velocity_y] + (gravity_dy * gravity * delta))
 
-        p[_position_x] = p[_position_x] + p[_velocity_dx] * delta
-        p[_position_y] = p[_position_y] + p[_velocity_dy] * delta
+        p[_position_x] = p[_position_x] + p[_velocity_x] * delta
+        p[_position_y] = p[_position_y] + p[_velocity_y] * delta
         
         p[_lifetime_elapsed] = p[_lifetime_elapsed] + delta
         local lifetime_t = p[_lifetime_elapsed] / p[_lifetime]
@@ -237,7 +242,7 @@ function ow.CheckpointParticles:_update_batch(batch, delta)
         end
 
         -- update angle
-        local angle = math.angle(p[_velocity_dx], p[_velocity_dy])
+        local angle = math.angle(p[_velocity_x], p[_velocity_y])
         p[_arc_min] = angle - arc_offset
         p[_arc_max] = angle + arc_offset
 
@@ -271,39 +276,43 @@ function ow.CheckpointParticles:_update_batch(batch, delta)
         table.insert(right, right_dx)
         table.insert(right, right_dy)
 
-        -- rebuild polygon tail
-        local polygon = p[_polygon]
-        if p[_path_length] < min_tail_length_factor * p[_radius] then
-            -- if too short, use default tri
-            table.clear(polygon)
+        local radius = p[_radius]
+        local path_t = math.min(1, p[_path_length] / (2 * radius))
 
-            local px, py = p[_position_x], p[_position_y]
-            local vdx, vdy = math.normalize(p[_velocity_dx], p[_velocity_dy])
+        do
+            local polygon = p[_polygon]
+            local threshold = 1.5 * radius
+            local use_fallback = p[_path_length] <= threshold
 
-            local left_vx, left_vy = math.turn_left(vdx, vdy)
-            local right_vx, right_vy = math.turn_right(vdx, vdy)
-
-            local radius = p[_radius]
-
-            table.insert(polygon, px + left_vx * radius)
-            table.insert(polygon, py + left_vy * radius)
-
-            table.insert(polygon, px - vdx * min_tail_length_factor * radius)
-            table.insert(polygon, py - vdy * min_tail_length_factor * radius)
-
-            table.insert(polygon, px + right_vx * radius)
-            table.insert(polygon, py + right_vy * radius)
-        else
-            -- else, use entire path
-            local radius = p[_radius]
             local n_nodes = #path / 2
+
+
+
+            local fallback_px, fallback_py, fallback_dx, fallback_dy, fallback_t_base
+            if use_fallback then
+                fallback_dx, fallback_dy = math.flip(math.normalize(p[_velocity_x], p[_velocity_y]))
+                if fallback_dx == 0 and fallback_dy == 0 then
+                    fallback_dx, fallback_dy = -gravity_dx, -gravity_dy
+                end
+            end
+
+            local function get_node(i)
+                if use_fallback then
+                    local praticle_i = math.floor(i / 2) + 1  -- 1-based node index from flat index
+                    local t = radius + 1 - (praticle_i - 1) / n_nodes * threshold
+                    return p[_position_x] + fallback_dx * t,
+                        p[_position_y] + fallback_dy * t
+                else
+                    return path[i], path[i + 1]
+                end
+            end
 
             local node_i = 1
             local easing_i = 0
             for i = #left - 1, 1, -2 do
-                local x, y = path[i+0], path[i+1]
+                local x, y = get_node(i)
 
-                local width = tail_easing(1 - math.min(1, easing_i / n_nodes)) * radius
+                local width = tail_easing(math.min(1, easing_i / n_nodes), path_t) * radius
                 easing_i = easing_i + 1
 
                 polygon[node_i+0] = x + left[i+0] * width
@@ -314,9 +323,9 @@ function ow.CheckpointParticles:_update_batch(batch, delta)
             easing_i = easing_i - 1
 
             for i = 1, #right, 2 do
-                local x, y = path[i+0], path[i+1]
+                local x, y = get_node(i)
 
-                local width = tail_easing(1 - math.min(1, easing_i / n_nodes)) * radius
+                local width = tail_easing(math.min(1, easing_i / n_nodes), path_t) * radius
                 easing_i = easing_i - 1
 
                 polygon[node_i+0] = x + right[i+0] * width
@@ -334,34 +343,44 @@ end
     --- @brief
 function ow.CheckpointParticles:_draw_batch(batch)
     if batch.settings.use_fill_color == true then
+        love.graphics.setLineWidth(1)
+        love.graphics.setLineStyle("rough")
+
         local fill_r, fill_g, fill_b, fill_a = rt.Palette.BLACK:unpack()
         for p in values(batch.particles) do
-            love.graphics.setColor(fill_r, fill_g, fill_b, fill_a * p[_color_a])
-            love.graphics.arc("fill", "closed",
-                p[_position_x], p[_position_y],
-                p[_radius],
-                p[_arc_min], p[_arc_max]
-            )
-            love.graphics.polygon("fill", p[_polygon])
+            if p[_is_disabled] ~= true then
+                love.graphics.setColor(fill_r, fill_g, fill_b, fill_a * p[_color_a])
+                love.graphics.arc("fill", "closed",
+                    p[_position_x], p[_position_y],
+                    p[_radius],
+                    p[_arc_min], p[_arc_max]
+                )
 
-            love.graphics.setColor(p[_color_r], p[_color_g], p[_color_b], p[_color_a])
-            love.graphics.arc("line", "open",
-                p[_position_x], p[_position_y],
-                p[_radius],
-                p[_arc_min], p[_arc_max]
-            )
-            love.graphics.line(p[_polygon])
+                love.graphics.polygon("fill", p[_polygon])
+
+                love.graphics.setColor(p[_color_r], p[_color_g], p[_color_b], p[_color_a])
+                love.graphics.arc("line", "open",
+                    p[_position_x], p[_position_y],
+                    p[_radius],
+                    p[_arc_min], p[_arc_max]
+                )
+
+                love.graphics.line(p[_polygon])
+            end
         end
     else
         for p in values(batch.particles) do
-            love.graphics.setColor(p[_color_r], p[_color_g], p[_color_b], p[_color_a])
-            love.graphics.polygon(p[_polygon])
-            love.graphics.arc("fill", "closed",
-                p[_position_x], p[_position_y],
-                p[_radius],
-                p[_arc_min], p[_arc_max]
-            )
-            love.graphics.polygon("fill", p[_polygon])
+            if p[_is_disabled] ~= true then
+                love.graphics.setColor(p[_color_r], p[_color_g], p[_color_b], p[_color_a])
+                love.graphics.polygon(p[_polygon])
+                love.graphics.arc("fill", "closed",
+                    p[_position_x], p[_position_y],
+                    p[_radius],
+                    p[_arc_min], p[_arc_max]
+                )
+
+                love.graphics.polygon("fill", p[_polygon])
+            end
         end
     end
 end
