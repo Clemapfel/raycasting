3dc87ee2069118c41adaaa9ad0d61225b78d273c
Clem Cords <mail@clemens-cords.com>
2026-07-05 00:30:36 +0200

    added love definitions

---
+++ love/typenames.lua
@@ -0,0 +1,158 @@
+--- @class love.BezierCurve
+love.BezierCurve = "BezierCurve"
+
+--- @class love.Body
+love.Body = "Body"
+
+--- @class love.ByteData
+love.ByteData = "ByteData"
+
+--- @class love.ChainShape
+love.ChainShape = "ChainShape"
+
+--- @class love.Channel
+love.Channel = "Channel"
+
+--- @class love.CircleShape
+love.CircleShape = "CircleShape"
+
+--- @class love.CompressedData
+love.CompressedData = "CompressedData"
+
+--- @class love.CompressedImageData
+love.CompressedImageData = "CompressedImageData"
+
+--- @class love.Contact
+love.Contact = "Contact"
+
+--- @class love.Cursor
+love.Cursor = "Cursor"
+
+--- @class love.Data
+love.Data = "Data"
+
+--- @class love.DataView
+love.DataView = "DataView"
+
+--- @class love.Decoder
+love.Decoder = "Decoder"
+
+--- @class love.DistanceJoint
+love.DistanceJoint = "DistanceJoint"
+
+--- @class love.Drawable
+love.Drawable = "Drawable"
+
+--- @class love.EdgeShape
+love.EdgeShape = "EdgeShape"
+
+--- @class love.File
+love.File = "File"
+
+--- @class love.FileData
+love.FileData = "FileData"
+
+--- @class love.Font
+love.Font = "Font"
+
+--- @class love.FrictionJoint
+love.FrictionJoint = "FrictionJoint"
+
+--- @class love.GearJoint
+love.GearJoint = "GearJoint"
+
+--- @class love.GlyphData
+love.GlyphData = "GlyphData"
+
+--- @class love.GraphicsBuffer
+love.GraphicsBuffer = "GraphicsBuffer"
+
+--- @class love.GraphicsReadback
+love.GraphicsReadback = "GraphicsReadback"
+
+--- @class love.ImageData
+love.ImageData = "ImageData"
+
+--- @class love.Joint
+love.Joint = "Joint"
+
+--- @class love.Joystick
+love.Joystick = "Joystick"
+
+--- @class love.Mesh
+love.Mesh = "Mesh"
+
+--- @class love.MotorJoint
+love.MotorJoint = "MotorJoint"
+
+--- @class love.MouseJoint
+love.MouseJoint = "MouseJoint"
+
+--- @class love.ParticleSystem
+love.ParticleSystem = "ParticleSystem"
+
+--- @class love.PolygonShape
+love.PolygonShape = "PolygonShape"
+
+--- @class love.PrismaticJoint
+love.PrismaticJoint = "PrismaticJoint"
+
+--- @class love.PulleyJoint
+love.PulleyJoint = "PulleyJoint"
+
+--- @class love.Quad
+love.Quad = "Quad"
+
+--- @class love.RandomGenerator
+love.RandomGenerator = "RandomGenerator"
+
+--- @class love.Rasterizer
+love.Rasterizer = "Rasterizer"
+
+--- @class love.RecordingDevice
+love.RecordingDevice = "RecordingDevice"
+
+--- @class love.RevoluteJoint
+love.RevoluteJoint = "RevoluteJoint"
+
+--- @class love.RopeJoint
+love.RopeJoint = "RopeJoint"
+
+--- @class love.Shader
+love.Shader = "Shader"
+
+--- @class love.Shape
+love.Shape = "Shape"
+
+--- @class love.SoundData
+love.SoundData = "SoundData"
+
+--- @class love.Source
+love.Source = "Source"
+
+--- @class love.SpriteBatch
+love.SpriteBatch = "SpriteBatch"
+
+--- @class love.TextBatch
+love.TextBatch = "TextBatch"
+
+--- @class love.Texture
+love.Texture = "Texture"
+
+--- @class love.Thread
+love.Thread = "Thread"
+
+--- @class love.Transform
+love.Transform = "Transform"
+
+--- @class love.Video
+love.Video = "Video"
+
+--- @class love.WeldJoint
+love.WeldJoint = "WeldJoint"
+
+--- @class love.WheelJoint
+love.WheelJoint = "WheelJoint"
+
+--- @class love.World
+love.World = "World"
